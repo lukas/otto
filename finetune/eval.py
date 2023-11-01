bf17d1c7fc69e676c56bf71b9a1b5030ef40225e
Thomas Capelle <thomas.capelle@steady-sun.com>
2023-11-01 12:28:46 +0000

    evaluation script

--- finetune/eval.py
+++ finetune/eval.py
@@ -1,3 +1,4 @@
+import argparse
 import wandb
 from types import SimpleNamespace
 from tqdm.auto import tqdm
@@ -10,68 +11,123 @@ from ft_utils import (
     load_model_from_artifact, 
     generate, 
     load_ds_from_artifact, 
-    create_mistral_instruc_prompt, 
+    create_mistral_instruct_prompt, 
     create_llama_prompt,
     create_llama_chat_prompt,
+    create_custom_prompt,
     load_model_from_hf,
 )
+
+def validate_prompt_format(prompt):
+    if not ("user" in prompt and "answer" in prompt):
+        raise Exception("The prompt is not formatted correctly, you need to provide: user, answer")
+    else:
+        return create_custom_prompt(prompt)
+
+def get_default_create_prompt(args):
+    "This should come from the model artifact metadata"
+    if "mistral" in args.MODEL_AT.lower():
+        create_prompt = create_mistral_instruct_prompt
+    elif "llama" in args.MODEL_AT.lower():
+        if "chat" in args.MODEL_AT.lower():
+            create_prompt = create_llama_chat_prompt
+        else:
+            create_prompt = create_llama_prompt
+    else:
+        raise Exception("Model not recognized")
+    return create_prompt
 
 defaults = SimpleNamespace(
     MODEL_AT = 'capecape/huggingface/6urzaw17-mistralai_Mistral-7B-Instruct-v0.1-ft:v0',
     DATASET_AT = 'capecape/otto/split_dataset:v2',
-    # MODEL_ID = None,  #  "mistralai/Mistral-7B-Instruct-v0.1" # Evaluate a model from the HF Hub
-    MODEL_ID = "meta-llama/Llama-2-7b-chat-hf"
+    MODEL_ID = None,
+    PROMPT = None,
 )
 
-def load_ds_and_model(defaults):
-    ds = load_ds_from_artifact(defaults.DATASET_AT)
-    if defaults.MODEL_ID is not None:
-        print(f"Loading pre-trained model, not finetuned one: {defaults.MODEL_ID}")
-        model, tokenizer = load_model_from_hf(defaults.MODEL_ID)
+def parse_args(defaults):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--MODEL_AT", type=str, default=defaults.MODEL_AT)
+    parser.add_argument("--DATASET_AT", type=str, default=defaults.DATASET_AT)
+    parser.add_argument("--MODEL_ID", type=str, default=defaults.MODEL_ID)
+    parser.add_argument("--PROMPT", type=str, default=defaults.PROMPT)
+    parser.add_argument("--PROMPT_FILE", type=str, default=None)
+    return parser.parse_args()
+
+def load_test_ds(args):
+    if args.PROMPT is not None: # custom prompt
+        create_prompt = validate_prompt_format(args.PROMPT)
     else:
-        model, tokenizer = load_model_from_artifact(defaults.MODEL_AT)
-        model = getattr(model, "model", model)  # maybe unwrap model
-    if "mistral" in model.name_or_path.lower():
-        create_prompt = create_mistral_instruc_prompt
-    elif "chat" in model.name_or_path.lower():
-        create_prompt = create_llama_chat_prompt
-    else:
-        create_prompt = create_llama_prompt
+        create_prompt = get_default_create_prompt(args)
+
+    # to parse the hf dataset
     create_test_prompt = lambda row: {"text": create_prompt({"user":row["user"], "answer":""})}
-    test_dataset = ds["test"].map(create_test_prompt)
     
-    return (model, tokenizer), test_dataset
+    ds = load_ds_from_artifact(args.DATASET_AT)
+    test_dataset = ds["test"].map(create_test_prompt)
+    return test_dataset
 
+def load_model(args):
+    if args.MODEL_ID is not None:  # load pre-trained model
+        print(f"Loading pre-trained model, not finetuned one: {args.MODEL_ID}")
+        model, tokenizer = load_model_from_hf(args.MODEL_ID)
+    else:
+        model, tokenizer = load_model_from_artifact(args.MODEL_AT)
+        model = getattr(model, "model", model)  # maybe unwrap model
+    
+    return model, tokenizer
 
-
-def create_predictions_table(model, tokenizer, test_dataset, max_new_tokens=256):
+def create_predictions_table(model, tokenizer, test_dataset, max_new_tokens=64):
     gen_config = GenerationConfig.from_pretrained(
         model.name_or_path,
         pad_token_id=tokenizer.eos_token_id,
         max_new_tokens=max_new_tokens)
 
     records_table = wandb.Table(columns=["prompt", "user", "answer", "generation"] + list(gen_config.to_dict().keys()))
-
+    acc = 0.
+    acc_lousy = 0.
     for example in tqdm(test_dataset, leave=False):
         prompt = example["text"]
+        user_query = example["user"]
+        label = example["answer"]
         generation = generate(prompt=prompt, 
                                model=model, 
                                tokenizer=tokenizer,
                                gen_config=gen_config)
-        print(f"{prompt} -> {generation}")
+        
+        print(f"{user_query} -> {generation}")
         records_table.add_data(prompt,
-                               example["user"],
-                               example["answer"],
-                               generation, *list(gen_config.to_dict().values()))
-    return records_table
+                               user_query,
+                               label,
+                               generation.strip(), 
+                               *list(gen_config.to_dict().values()))
+        
+        if generation.strip() == label: acc += 1
+        if label.lower() in generation.lower(): acc_lousy +=1
+    
+    return records_table, acc/len(test_dataset), acc_lousy/len(test_dataset)
 
-if __name__ == "__main__":
+def evaluate(args):
     # initialize run
-    wandb.init(project="otto", job_type="eval")
+    wandb.init(project="otto", job_type="eval", config=args)
 
-    (model, tokenizer), test_dataset = load_ds_and_model(defaults)
+    test_dataset = load_test_ds(args)
+    model, tokenizer = load_model(args)
 
-    table = create_predictions_table(model, tokenizer, test_dataset, 64)
+    table, acc, acc_lousy = create_predictions_table(model, tokenizer, test_dataset, 64)
 
     wandb.log({"eval_predictions":table})
+    wandb.run.summary["acc"] = acc
+    wandb.run.summary["acc_lousy"] = acc_lousy
     wandb.finish()
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        prompt = f.read()
+    return prompt
+    
+if __name__ == "__main__":
+    args = parse_args(defaults)
+    if args.PROMPT_FILE is not None:
+        print(f"Reading prompt from file: {args.PROMPT_FILE}")
+        args.PROMPT = read_file(args.PROMPT_FILE)
+    evaluate(args)
